8585270a7546cfdcbba29eeb50ac5a5db7ee009b
Jose Tomas <josetomascodecido@gmail.com>
2018-09-04 20:02:56 -0300

    first commit

---
+++ app/models/proyect.rb
@@ -0,0 +1,4 @@
+class Proyect < ApplicationRecord
+  has_many :allocations
+  has_many :workers,  through: :allocations
+end
---
+++ db/migrate/20180904225349_create_workers.rb
@@ -0,0 +1,11 @@
+class CreateWorkers < ActiveRecord::Migration[5.2]
+  def change
+    create_table :workers do |t|
+      t.string :name
+      t.string :last_name
+      t.string :email
+
+      t.timestamps
+    end
+  end
+end
